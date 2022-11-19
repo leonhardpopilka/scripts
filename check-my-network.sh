a05571579327e15b37b0f44b19d5e8010f0c585f
grandlodin <leonhardpopilka@googlemail.com>
2022-11-19 14:19:50 +0100

    Stuff in check-my-network.sh

--- check-my-network.sh
+++ check-my-network.sh
@@ -2,15 +2,44 @@
 
 # This script looks at my local network and tells me things about it.
 
-# install "Nmap" 
-echo "INSTALLING STUFF..."
-sudo apt install net-tools
-# $(sudo apt-get install nmap -y)
-echo "INSTALLATION DONE."
-echo "---"
+
+if [[ $OSTYPE =~ ^linux ]]; then
+    echo "Hello from linux."
+    echo "INSTALLING STUFF..."
+    echo "Do you want to update stuff? (y/n)"
+    read update
+    if [[ $update == 'y' ]]; then
+        sudo apt update
+        sudo apt upgrade
+        sudo apt-get update
+        sudo apt autoremove
+        sudo apt install net-tools
+        sudo apt-get install nmap -y
+        echo "INSTALLATION DONE."
+        echo "Press ENTER to continue ..."
+        read pause
+        clear
+    fi
+else
+# [[ $OSTYPE =~ ^msys ]]; then
+    echo "Hello WINDOWS or MAC"
+    echo "This doesn't work here :("
+    exit
+fi
 
 # check my ip adresses
 echo "NETWORK:"
+ipadress=$(ifconfig | grep broadcast | awk '{print $2}')
+echo "Machine IP: $ipadress"
+read A B C D <<<"${ipadress//./ }"
+echo ""
+echo "running arp -a ..."
 arp -a
+echo ""
+echo "running nmap -sP $A.$B.$C.0/24..."
+nmap -sP $A.$B.$C.0/24
+echo ""
+echo "running ifconfig..."
+ifconfig
 
 exit
